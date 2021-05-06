4d97695c2dece4cf5a5eae498df55f74d37f853a
nando <muhammad.rezki.ananda@gmail.com>
2021-05-06 23:32:42 +0700

    Project Setup

--- app/src/main/java/com/newbiexpert/android_kotlin_navigationarchitecture/MainActivity.kt
+++ app/src/main/java/com/newbiexpert/android_kotlin_navigationarchitecture/MainActivity.kt
@@ -2,10 +2,13 @@ package com.newbiexpert.android_kotlin_navigationarchitecture
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.newbiexpert.android_kotlin_navigationarchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
